88572b354b4c6f80155bbf385e6d2cedd49fba8a
efierr <erickfierro0413@gmail.com>
2024-06-29 16:41:48 -0400

    now able to look for a specific workout with id , created route for wokrout/id

--- controllers/workoutController.js
+++ controllers/workoutController.js
@@ -11,6 +11,15 @@ router.get("/", (req,res) => {
     res.status(200).send(workouts)
 });
 
+router.get("/:id", (req, res) => {
+    const {id} = req.params
+    const workout = workouts.find((ele) => ele.id === +id)
 
+    if (workout) {
+        res.status(200).send(workout)
+    } else {
+        res.status(404).json({error: "help is on the way"})
+    }
+})
 
 module.exports = router;
